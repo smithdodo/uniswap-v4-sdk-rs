5d1e457d35dde5b30995a748dde00111db9bf404
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-10-20 06:27:48 -0400

    feat: update fee type from `u32` to `U24` and extend pool functionality
    Changed fee type from `u32` to `U24` for better specificity. Added new pool methods, including price computation, currency involvement checks, and swap operations.

--- src/entities/pool.rs
+++ src/entities/pool.rs
@@ -1,16 +1,16 @@
 use crate::prelude::*;
-use alloy_primitives::{aliases::U24, keccak256, Address, B256, U160};
+use alloy_primitives::{aliases::U24, keccak256, uint, Address, ChainId, B256, I256, U160};
 use alloy_sol_types::SolValue;
 use uniswap_sdk_core::prelude::*;
 use uniswap_v3_sdk::prelude::*;
 
-pub const DYANMIC_FEE_FLAG: u32 = 0x800000;
+pub const DYANMIC_FEE_FLAG: U24 = uint!(0x800000_U24);
 
 #[derive(Clone, Debug)]
 pub struct PoolKey<I: TickIndex> {
     currency0: Address,
     currency1: Address,
-    fee: u32,
+    fee: U24,
     tick_spacing: I,
     hooks: Address,
 }
@@ -23,7 +23,7 @@ where
 {
     pub currency0: Currency,
     pub currency1: Currency,
-    pub fee: u32,
+    pub fee: U24,
     pub tick_spacing: TP::Index,
     pub sqrt_ratio_x96: U160,
     pub hooks: Address,
@@ -50,7 +50,7 @@ impl Pool {
     pub fn get_pool_key<I: TickIndex>(
         currency_a: &Currency,
         currency_b: &Currency,
-        fee: u32,
+        fee: U24,
         tick_spacing: I,
         hooks: Address,
     ) -> PoolKey<I> {
@@ -67,7 +67,7 @@ impl Pool {
     pub fn get_pool_id<I: TickIndex>(
         currency_a: &Currency,
         currency_b: &Currency,
-        fee: u32,
+        fee: U24,
         tick_spacing: I,
         hooks: Address,
     ) -> B256 {
@@ -99,7 +99,7 @@ impl Pool {
     pub fn new(
         currency_a: Currency,
         currency_b: Currency,
-        fee: u32,
+        fee: U24,
         tick_spacing: <NoTickDataProvider as TickDataProvider>::Index,
         hooks: Address,
         sqrt_ratio_x96: U160,
@@ -135,14 +135,14 @@ impl<TP: TickDataProvider> Pool<TP> {
     pub fn new_with_tick_data_provider(
         currency_a: Currency,
         currency_b: Currency,
-        fee: u32,
+        fee: U24,
         tick_spacing: TP::Index,
         hooks: Address,
         sqrt_ratio_x96: U160,
         liquidity: u128,
         tick_data_provider: TP,
     ) -> Self {
-        assert!(fee == DYANMIC_FEE_FLAG || fee < 1_000_000, "FEE");
+        assert!(fee == DYANMIC_FEE_FLAG || fee < uint!(1_000_000_U24), "FEE");
         if fee == DYANMIC_FEE_FLAG {
             assert_ne!(hooks, Address::ZERO, "Dynamic fee pool requires a hook");
         }
@@ -172,5 +172,235 @@ impl<TP: TickDataProvider> Pool<TP> {
             pool_key,
             pool_id,
         }
+    }
+
+    pub fn token0(&self) -> &Currency {
+        &self.currency0
+    }
+
+    pub fn token1(&self) -> &Currency {
+        &self.currency1
+    }
+
+    /// Returns true if the currency is either currency0 or currency1
+    ///
+    /// ## Arguments
+    ///
+    /// * `currency`: The currency to check
+    pub fn involves_currency(&self, currency: &impl BaseCurrency) -> bool {
+        self.currency0.equals(currency) || self.currency1.equals(currency)
+    }
+
+    pub fn involves_token(&self, currency: &impl BaseCurrency) -> bool {
+        self.involves_currency(currency)
+    }
+
+    /// Returns the current mid price of the pool in terms of currency0, i.e. the ratio of currency1
+    /// over currency0
+    pub fn currency0_price(&self) -> Price<Currency, Currency> {
+        let sqrt_ratio_x96 = self.sqrt_ratio_x96.to_big_uint();
+        Price::new(
+            self.currency0.clone(),
+            self.currency1.clone(),
+            Q192.to_big_int(),
+            &sqrt_ratio_x96 * &sqrt_ratio_x96,
+        )
+    }
+
+    pub fn token0_price(&self) -> Price<Currency, Currency> {
+        self.currency0_price()
+    }
+
+    /// Returns the current mid price of the pool in terms of currency1, i.e. the ratio of currency0
+    /// over currency1
+    pub fn currency1_price(&self) -> Price<Currency, Currency> {
+        let sqrt_ratio_x96 = self.sqrt_ratio_x96.to_big_uint();
+        Price::new(
+            self.currency1.clone(),
+            self.currency0.clone(),
+            &sqrt_ratio_x96 * &sqrt_ratio_x96,
+            Q192.to_big_int(),
+        )
+    }
+
+    pub fn token1_price(&self) -> Price<Currency, Currency> {
+        self.currency1_price()
+    }
+
+    /// Return the price of the given currency in terms of the other currency in the pool.
+    ///
+    /// ## Arguments
+    ///
+    /// * `currency`: The currency to return price of
+    pub fn price_of(&self, currency: &impl BaseCurrency) -> Price<Currency, Currency> {
+        assert!(self.involves_currency(currency), "CURRENCY");
+        if self.currency0.equals(currency) {
+            self.currency0_price()
+        } else {
+            self.currency1_price()
+        }
+    }
+
+    /// Returns the chain ID of the currencies in the pool.
+    pub fn chain_id(&self) -> ChainId {
+        self.currency0.chain_id()
+    }
+
+    /// Executes a swap
+    ///
+    /// ## Arguments
+    ///
+    /// * `zero_for_one`: Whether the amount in is token0 or token1
+    /// * `amount_specified`: The amount of the swap, which implicitly configures the swap as exact
+    ///   input (positive), or exact output (negative)
+    /// * `sqrt_price_limit_x96`: The Q64.96 sqrt price limit. If zero for one, the price cannot be
+    ///   less than this value after the swap. If one for zero, the price cannot be greater than
+    ///   this value after the swap
+    fn swap(
+        &self,
+        zero_for_one: bool,
+        amount_specified: I256,
+        sqrt_price_limit_x96: Option<U160>,
+    ) -> SwapState<TP::Index> {
+        if self.non_impactful_hook() {
+            v3_swap(
+                self.fee,
+                self.sqrt_ratio_x96,
+                self.tick_current,
+                self.liquidity,
+                self.tick_spacing,
+                &self.tick_data_provider,
+                zero_for_one,
+                amount_specified,
+                sqrt_price_limit_x96,
+            )
+            .unwrap()
+        } else {
+            panic!("Unsupported hook");
+        }
+    }
+
+    fn non_impactful_hook(&self) -> bool {
+        self.hooks == Address::ZERO
+    }
+}
+
+impl<TP: Clone + TickDataProvider> Pool<TP> {
+    /// Given an input amount of a token, return the computed output amount, and a pool with state
+    /// updated after the trade
+    ///
+    /// ## Note
+    ///
+    /// Works only for vanilla hookless v3 pools, otherwise throws an error
+    ///
+    /// ## Arguments
+    ///
+    /// * `input_amount`: The input amount for which to quote the output amount
+    /// * `sqrt_price_limit_x96`: The Q64.96 sqrt price limit
+    ///
+    /// returns: The output amount and the pool with updated state
+    pub fn get_output_amount(
+        &self,
+        input_amount: &CurrencyAmount<impl BaseCurrency>,
+        sqrt_price_limit_x96: Option<U160>,
+    ) -> (CurrencyAmount<&Currency>, Self) {
+        assert!(self.involves_currency(&input_amount.currency), "CURRENCY");
+
+        let zero_for_one = input_amount.currency.equals(&self.currency0);
+
+        let SwapState {
+            amount_specified_remaining,
+            amount_calculated: output_amount,
+            sqrt_price_x96,
+            liquidity,
+            ..
+        } = self.swap(
+            zero_for_one,
+            I256::from_big_int(input_amount.quotient()),
+            sqrt_price_limit_x96,
+        );
+
+        if !amount_specified_remaining.is_zero() && sqrt_price_limit_x96.is_none() {
+            panic!("Insufficient liquidity");
+        }
+
+        let output_currency = if zero_for_one {
+            &self.currency1
+        } else {
+            &self.currency0
+        };
+        (
+            CurrencyAmount::from_raw_amount(output_currency, -output_amount.to_big_int()).unwrap(),
+            Pool::new_with_tick_data_provider(
+                self.currency0.clone(),
+                self.currency1.clone(),
+                self.fee,
+                self.tick_spacing,
+                self.hooks,
+                sqrt_price_x96,
+                liquidity,
+                self.tick_data_provider.clone(),
+            ),
+        )
+    }
+
+    /// Given a desired output amount of a currency, return the computed input amount and a pool
+    /// with state updated after the trade
+    ///
+    /// ## Note
+    ///
+    /// Works only for vanilla hookless v3 pools, otherwise throws an error
+    ///
+    /// ## Arguments
+    ///
+    /// * `output_amount`: The output amount for which to quote the input amount
+    /// * `sqrt_price_limit_x96`: The Q64.96 sqrt price limit. If zero for one, the price cannot be
+    ///   less than this value after the swap. If one for zero, the price cannot be greater than
+    ///   this value after the swap
+    ///
+    /// returns: The input amount and the pool with updated state
+    pub fn get_input_amount(
+        &self,
+        output_amount: &CurrencyAmount<impl BaseCurrency>,
+        sqrt_price_limit_x96: Option<U160>,
+    ) -> (CurrencyAmount<&Currency>, Self) {
+        assert!(self.involves_currency(&output_amount.currency), "CURRENCY");
+
+        let zero_for_one = output_amount.currency.equals(&self.currency1);
+
+        let SwapState {
+            amount_specified_remaining,
+            amount_calculated: input_amount,
+            sqrt_price_x96,
+            liquidity,
+            ..
+        } = self.swap(
+            zero_for_one,
+            I256::from_big_int(-output_amount.quotient()),
+            sqrt_price_limit_x96,
+        );
+
+        if !amount_specified_remaining.is_zero() && sqrt_price_limit_x96.is_none() {
+            panic!("Insufficient liquidity");
+        }
+
+        let input_currency = if zero_for_one {
+            &self.currency0
+        } else {
+            &self.currency1
+        };
+        (
+            CurrencyAmount::from_raw_amount(input_currency, input_amount.to_big_int()).unwrap(),
+            Pool::new_with_tick_data_provider(
+                self.currency0.clone(),
+                self.currency1.clone(),
+                self.fee,
+                self.tick_spacing,
+                self.hooks,
+                sqrt_price_x96,
+                liquidity,
+                self.tick_data_provider.clone(),
+            ),
+        )
     }
 }
