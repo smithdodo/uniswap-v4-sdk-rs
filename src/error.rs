87d9d169a0f563ccdc30e6f19b9820d35c13cbb2
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-11-07 00:26:49 -0500

    chore: update dependencies and refactor error handling (#7)
    Commented out `derive_more` dependency and updated versions for `thiserror`, `uniswap-sdk-core`, and `uniswap-v3-sdk` in `Cargo.toml`. Refactored error handling in `src/error.rs` to remove `derive_more` and improve error attribute usage.

--- src/error.rs
+++ src/error.rs
@@ -1,39 +1,39 @@
+#![allow(clippy::missing_inline_in_public_items)]
+
 #[cfg(doc)]
 use crate::prelude::*;
 
 use alloy_sol_types::Error as SolError;
-use derive_more::From;
 use uniswap_sdk_core::error::Error as CoreError;
 use uniswap_v3_sdk::error::Error as V3Error;
 
-#[derive(Debug, From)]
-#[cfg_attr(feature = "std", derive(thiserror::Error))]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// Thrown when an error occurs in the core library.
-    #[cfg_attr(feature = "std", error("{0}"))]
-    Core(#[cfg_attr(not(feature = "std"), from)] CoreError),
+    #[error("{0}")]
+    Core(#[from] CoreError),
 
     /// Thrown when an error occurs in the v3 library.
-    #[cfg_attr(feature = "std", error("{0}"))]
-    V3(#[cfg_attr(not(feature = "std"), from)] V3Error),
+    #[error("{0}")]
+    V3(#[from] V3Error),
 
     /// Thrown when an error occurs in the sol types library.
-    #[cfg_attr(feature = "std", error("{0}"))]
-    Sol(#[cfg_attr(not(feature = "std"), from)] SolError),
+    #[error("{0}")]
+    Sol(#[from] SolError),
 
     /// Thrown when the action is not supported.
-    #[cfg_attr(feature = "std", error("Unsupported action {0}"))]
+    #[error("Unsupported action {0}")]
     InvalidAction(u8),
 
     /// Thrown when the currency passed to [`get_path_currency`] is not one of the pool's
     /// currencies.
-    #[cfg_attr(feature = "std", error("Invalid currency"))]
+    #[error("Invalid currency")]
     InvalidCurrency,
 
     /// Thrown when trying to simulate a swap with an unsupported hook.
-    #[cfg_attr(feature = "std", error("Unsupported hook"))]
+    #[error("Unsupported hook")]
     UnsupportedHook,
 
-    #[cfg_attr(feature = "std", error("Insufficient liquidity"))]
+    #[error("Insufficient liquidity")]
     InsufficientLiquidity,
 }
