e97b389b557b3861bc77bc2c8b4d35ad473e03d1
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-11-09 22:37:45 -0500

    test: add unit tests for `Route`
    Introduce comprehensive unit tests for the `Route` entity. These tests cover various scenarios, including constructing routes, handling paths with currencies, and calculating mid prices.

--- src/entities/route.rs
+++ src/entities/route.rs
@@ -120,3 +120,371 @@ where
         Ok(mid_price)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{Pool, Route};
+    use crate::tests::*;
+    use alloy_primitives::address;
+    use once_cell::sync::Lazy;
+    use uniswap_sdk_core::{prelude::*, token};
+    use uniswap_v3_sdk::prelude::*;
+
+    static CURRENCY0: Lazy<Currency> =
+        Lazy::new(|| token!(1, "0000000000000000000000000000000000000001", 18, "t0").into());
+    static CURRENCY1: Lazy<Currency> =
+        Lazy::new(|| token!(1, "0000000000000000000000000000000000000002", 18, "t1").into());
+    static CURRENCY2: Lazy<Currency> =
+        Lazy::new(|| token!(1, "0000000000000000000000000000000000000003", 18, "t2").into());
+    static POOL_0_1: Lazy<Pool> = Lazy::new(|| {
+        Pool::new(
+            CURRENCY0.clone(),
+            CURRENCY1.clone(),
+            FeeAmount::MEDIUM.into(),
+            10,
+            Address::ZERO,
+            encode_sqrt_ratio_x96(1, 1),
+            0,
+        )
+        .unwrap()
+    });
+    static POOL_0_ETH: Lazy<Pool> = Lazy::new(|| {
+        Pool::new(
+            CURRENCY0.clone(),
+            ETHER.clone().into(),
+            FeeAmount::MEDIUM.into(),
+            10,
+            Address::ZERO,
+            encode_sqrt_ratio_x96(1, 1),
+            0,
+        )
+        .unwrap()
+    });
+    static POOL_1_ETH: Lazy<Pool> = Lazy::new(|| {
+        Pool::new(
+            CURRENCY1.clone(),
+            ETHER.clone().into(),
+            FeeAmount::MEDIUM.into(),
+            10,
+            Address::ZERO,
+            encode_sqrt_ratio_x96(1, 1),
+            0,
+        )
+        .unwrap()
+    });
+    static POOL_0_WETH: Lazy<Pool> = Lazy::new(|| {
+        Pool::new(
+            CURRENCY0.clone(),
+            WETH.clone().into(),
+            FeeAmount::MEDIUM.into(),
+            10,
+            Address::ZERO,
+            encode_sqrt_ratio_x96(1, 1),
+            0,
+        )
+        .unwrap()
+    });
+    static POOL_ETH_WETH: Lazy<Pool> = Lazy::new(|| {
+        Pool::new(
+            ETHER.clone().into(),
+            WETH.clone().into(),
+            FeeAmount::MEDIUM.into(),
+            10,
+            Address::ZERO,
+            encode_sqrt_ratio_x96(1, 1),
+            0,
+        )
+        .unwrap()
+    });
+
+    mod path {
+        use super::*;
+
+        #[test]
+        fn constructs_a_path_from_the_currencies() {
+            let route =
+                Route::new(vec![POOL_0_1.clone()], CURRENCY0.clone(), CURRENCY1.clone()).unwrap();
+            assert_eq!(route.pools, vec![POOL_0_1.clone()]);
+            assert_eq!(
+                route.currency_path(),
+                vec![CURRENCY0.clone(), CURRENCY1.clone()]
+            );
+            assert_eq!(route.input, CURRENCY0.clone());
+            assert_eq!(route.output, CURRENCY1.clone());
+            assert_eq!(route.chain_id(), 1);
+        }
+
+        #[test]
+        #[should_panic(expected = "InvalidCurrency")]
+        fn should_fail_if_the_input_is_not_in_the_first_pool() {
+            Route::new(vec![POOL_0_1.clone()], ETHER.clone(), CURRENCY1.clone()).unwrap();
+        }
+
+        #[test]
+        #[should_panic(expected = "InvalidCurrency")]
+        fn should_fail_if_the_output_is_not_in_the_last_pool() {
+            Route::new(vec![POOL_0_1.clone()], CURRENCY0.clone(), ETHER.clone()).unwrap();
+        }
+    }
+
+    #[test]
+    fn can_have_a_currency_as_both_input_and_output() {
+        let route = Route::new(
+            vec![POOL_0_ETH.clone(), POOL_0_1.clone(), POOL_1_ETH.clone()],
+            ETHER.clone(),
+            ETHER.clone(),
+        )
+        .unwrap();
+        assert_eq!(
+            route.pools,
+            vec![POOL_0_ETH.clone(), POOL_0_1.clone(), POOL_1_ETH.clone()]
+        );
+        assert_eq!(route.input, ETHER.clone());
+        assert_eq!(route.output, ETHER.clone());
+    }
+
+    #[test]
+    fn supports_ether_input() {
+        let route = Route::new(vec![POOL_0_ETH.clone()], ETHER.clone(), CURRENCY0.clone()).unwrap();
+        assert_eq!(route.pools, vec![POOL_0_ETH.clone()]);
+        assert_eq!(route.input, ETHER.clone());
+        assert_eq!(route.output, CURRENCY0.clone());
+    }
+
+    #[test]
+    fn supports_ether_output() {
+        let route = Route::new(vec![POOL_0_ETH.clone()], CURRENCY0.clone(), ETHER.clone()).unwrap();
+        assert_eq!(route.pools, vec![POOL_0_ETH.clone()]);
+        assert_eq!(route.input, CURRENCY0.clone());
+        assert_eq!(route.output, ETHER.clone());
+    }
+
+    #[test]
+    #[should_panic(expected = "PATH")]
+    fn does_not_support_weth_to_eth_conversion_without_trading_through_an_eth_to_weth_pool() {
+        Route::new(
+            vec![POOL_0_WETH.clone(), POOL_1_ETH.clone()],
+            CURRENCY0.clone(),
+            CURRENCY1.clone(),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "PATH")]
+    fn does_not_support_eth_to_weth_conversion_without_trading_through_an_eth_to_weth_pool() {
+        Route::new(
+            vec![POOL_1_ETH.clone(), POOL_0_WETH.clone()],
+            CURRENCY1.clone(),
+            CURRENCY0.clone(),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn supports_trading_through_eth_weth_pools() {
+        let route = Route::new(
+            vec![
+                POOL_0_WETH.clone(),
+                POOL_ETH_WETH.clone(),
+                POOL_1_ETH.clone(),
+            ],
+            CURRENCY0.clone(),
+            CURRENCY1.clone(),
+        )
+        .unwrap();
+        assert_eq!(
+            route.pools,
+            vec![
+                POOL_0_WETH.clone(),
+                POOL_ETH_WETH.clone(),
+                POOL_1_ETH.clone()
+            ]
+        );
+        assert_eq!(route.input, CURRENCY0.clone());
+        assert_eq!(route.output, CURRENCY1.clone());
+    }
+
+    mod mid_price {
+        use super::*;
+
+        static POOL_0_1: Lazy<Pool> = Lazy::new(|| {
+            Pool::new(
+                CURRENCY0.clone(),
+                CURRENCY1.clone(),
+                FeeAmount::MEDIUM.into(),
+                10,
+                Address::ZERO,
+                encode_sqrt_ratio_x96(1, 5),
+                0,
+            )
+            .unwrap()
+        });
+        static POOL_1_2: Lazy<Pool> = Lazy::new(|| {
+            Pool::new(
+                CURRENCY1.clone(),
+                CURRENCY2.clone(),
+                FeeAmount::MEDIUM.into(),
+                10,
+                Address::ZERO,
+                encode_sqrt_ratio_x96(15, 30),
+                0,
+            )
+            .unwrap()
+        });
+        static POOL_0_ETH: Lazy<Pool> = Lazy::new(|| {
+            Pool::new(
+                CURRENCY0.clone(),
+                ETHER.clone().into(),
+                FeeAmount::MEDIUM.into(),
+                10,
+                Address::ZERO,
+                encode_sqrt_ratio_x96(3, 1),
+                0,
+            )
+            .unwrap()
+        });
+        static POOL_1_ETH: Lazy<Pool> = Lazy::new(|| {
+            Pool::new(
+                CURRENCY1.clone(),
+                ETHER.clone().into(),
+                FeeAmount::MEDIUM.into(),
+                10,
+                Address::ZERO,
+                encode_sqrt_ratio_x96(1, 7),
+                0,
+            )
+            .unwrap()
+        });
+
+        #[test]
+        fn correct_for_0_to_1() {
+            let route =
+                Route::new(vec![POOL_0_1.clone()], CURRENCY0.clone(), CURRENCY1.clone()).unwrap();
+            let price = route.mid_price().unwrap();
+            assert_eq!(price.to_fixed(4, None), "0.2000");
+            assert!(price.base_currency.equals(&CURRENCY0.clone()));
+            assert!(price.quote_currency.equals(&CURRENCY1.clone()));
+        }
+
+        #[test]
+        fn is_cached() {
+            let mut route =
+                Route::new(vec![POOL_0_1.clone()], CURRENCY0.clone(), CURRENCY1.clone()).unwrap();
+            let mid_price = route.mid_price_cached().unwrap();
+            assert_eq!(mid_price, route.mid_price_cached().unwrap());
+        }
+
+        #[test]
+        fn correct_for_1_to_0() {
+            let route =
+                Route::new(vec![POOL_0_1.clone()], CURRENCY1.clone(), CURRENCY0.clone()).unwrap();
+            let price = route.mid_price().unwrap();
+            assert_eq!(price.to_fixed(4, None), "5.0000");
+            assert!(price.base_currency.equals(&CURRENCY1.clone()));
+            assert!(price.quote_currency.equals(&CURRENCY0.clone()));
+        }
+
+        #[test]
+        fn correct_for_0_to_1_to_2() {
+            let route = Route::new(
+                vec![POOL_0_1.clone(), POOL_1_2.clone()],
+                CURRENCY0.clone(),
+                CURRENCY2.clone(),
+            )
+            .unwrap();
+            let price = route.mid_price().unwrap();
+            assert_eq!(price.to_fixed(4, None), "0.1000");
+            assert!(price.base_currency.equals(&CURRENCY0.clone()));
+            assert!(price.quote_currency.equals(&CURRENCY2.clone()));
+        }
+
+        #[test]
+        fn correct_for_2_to_1_to_0() {
+            let route = Route::new(
+                vec![POOL_1_2.clone(), POOL_0_1.clone()],
+                CURRENCY2.clone(),
+                CURRENCY0.clone(),
+            )
+            .unwrap();
+            let price = route.mid_price().unwrap();
+            assert_eq!(price.to_fixed(4, None), "10.0000");
+            assert!(price.base_currency.equals(&CURRENCY2.clone()));
+            assert!(price.quote_currency.equals(&CURRENCY0.clone()));
+        }
+
+        #[test]
+        fn correct_for_ether_to_0() {
+            let route =
+                Route::new(vec![POOL_0_ETH.clone()], ETHER.clone(), CURRENCY0.clone()).unwrap();
+            let price = route.mid_price().unwrap();
+            assert_eq!(price.to_fixed(4, None), "3.0000");
+            assert!(price.base_currency.equals(&ETHER.clone()));
+            assert!(price.quote_currency.equals(&CURRENCY0.clone()));
+        }
+
+        #[test]
+        fn correct_for_1_to_eth() {
+            let route =
+                Route::new(vec![POOL_1_ETH.clone()], CURRENCY1.clone(), ETHER.clone()).unwrap();
+            let price = route.mid_price().unwrap();
+            assert_eq!(price.to_fixed(4, None), "7.0000");
+            assert!(price.base_currency.equals(&CURRENCY1.clone()));
+            assert!(price.quote_currency.equals(&ETHER.clone()));
+        }
+
+        #[test]
+        fn correct_for_ether_to_0_to_1_to_eth() {
+            let route = Route::new(
+                vec![POOL_0_ETH.clone(), POOL_0_1.clone(), POOL_1_ETH.clone()],
+                ETHER.clone(),
+                ETHER.clone(),
+            )
+            .unwrap();
+            let price = route.mid_price().unwrap();
+            assert_eq!(price.to_significant(4, None).unwrap(), "4.2");
+            assert!(price.base_currency.equals(&ETHER.clone()));
+            assert!(price.quote_currency.equals(&ETHER.clone()));
+        }
+
+        #[test]
+        fn can_be_constructed_with_ether_as_input_on_a_weth_pool() {
+            let route =
+                Route::new(vec![POOL_0_WETH.clone()], ETHER.clone(), CURRENCY0.clone()).unwrap();
+            assert_eq!(route.input, ETHER.clone());
+            assert_eq!(route.path_input, WETH.clone().into());
+            assert_eq!(route.output, CURRENCY0.clone());
+            assert_eq!(route.path_output, CURRENCY0.clone());
+        }
+
+        #[test]
+        fn can_be_constructed_with_weth_as_input_on_a_eth_pool() {
+            let route =
+                Route::new(vec![POOL_0_ETH.clone()], WETH.clone(), CURRENCY0.clone()).unwrap();
+            assert_eq!(route.input, WETH.clone());
+            assert_eq!(route.path_input, ETHER.clone().into());
+            assert_eq!(route.output, CURRENCY0.clone());
+            assert_eq!(route.path_output, CURRENCY0.clone());
+        }
+
+        #[test]
+        fn can_be_constructed_with_ether_as_output_on_a_weth_pool() {
+            let route =
+                Route::new(vec![POOL_0_WETH.clone()], CURRENCY0.clone(), ETHER.clone()).unwrap();
+            assert_eq!(route.input, CURRENCY0.clone());
+            assert_eq!(route.path_input, CURRENCY0.clone());
+            assert_eq!(route.output, ETHER.clone());
+            assert_eq!(route.path_output, WETH.clone().into());
+        }
+
+        #[test]
+        fn can_be_constructed_with_weth_as_output_on_a_eth_pool() {
+            let route =
+                Route::new(vec![POOL_0_ETH.clone()], CURRENCY0.clone(), WETH.clone()).unwrap();
+            assert_eq!(route.input, CURRENCY0.clone());
+            assert_eq!(route.path_input, CURRENCY0.clone());
+            assert_eq!(route.output, WETH.clone());
+            assert_eq!(route.path_output, ETHER.clone().into());
+        }
+    }
+}
